2ad183da993302bd9e1847417667eb7bbcd6b52c
eriksgda <erik.ksgda@gmail.com>
2024-08-24 23:04:53 -0300

    remove blank lines in company repository

--- src/main/java/com/example/JobManagement/company/CompanyRepository.java
+++ src/main/java/com/example/JobManagement/company/CompanyRepository.java
@@ -1,9 +1,7 @@
 package com.example.JobManagement.company;
 
-import com.example.JobManagement.candidate.CandidateEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
 import java.util.UUID;
 
 public interface CompanyRepository extends JpaRepository<CompanyEntity, UUID> {
