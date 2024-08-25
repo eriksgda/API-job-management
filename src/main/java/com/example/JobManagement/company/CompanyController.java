41681c856df9d02ceb8f3568cb4334d2ff3d0648
eriksgda <erik.ksgda@gmail.com>
2024-08-24 23:05:36 -0300

    create company controller

---
+++ src/main/java/com/example/JobManagement/company/CompanyController.java
@@ -0,0 +1,29 @@
+package com.example.JobManagement.company;
+
+import com.example.JobManagement.exceptions.UserOrEmailAlreadyExistException;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/company")
+public class CompanyController {
+
+    @Autowired
+    private CompanyService service;
+
+    @PostMapping("/")
+    public ResponseEntity<Object> createCompany(@Valid @RequestBody CompanyEntity company){
+        try {
+            CompanyEntity result = this.service.createCompany(company);
+            return ResponseEntity.ok().body(result);
+        } catch (UserOrEmailAlreadyExistException exception) {
+            return ResponseEntity.badRequest().body(exception.getMessage());
+        }
+
+    }
+}
