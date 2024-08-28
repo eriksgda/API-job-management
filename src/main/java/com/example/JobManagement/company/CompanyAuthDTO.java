3f6943bbe329e4df35ef83ebad2b3b638046ca8c
eriksgda <erik.ksgda@gmail.com>
2024-08-28 17:33:47 -0300

    create company auth dto

---
+++ src/main/java/com/example/JobManagement/company/CompanyAuthDTO.java
@@ -0,0 +1,14 @@
+package com.example.JobManagement.company;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class CompanyAuthDTO {
+
+    private String username;
+    private String password;
+}
