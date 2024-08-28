52fca6f38d5645717cdcab0c4d1225659f934c7e
eriksgda <erik.ksgda@gmail.com>
2024-08-28 17:31:06 -0300

    create security filter

---
+++ src/main/java/com/example/JobManagement/security/SecurityFilter.java
@@ -0,0 +1,44 @@
+package com.example.JobManagement.security;
+
+import com.example.JobManagement.security.providers.JWTProvider;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Collections;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private JWTProvider jwtProvider;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+
+        SecurityContextHolder.getContext().setAuthentication(null);
+        String header = request.getHeader("Authorization");
+
+        if (header != null){
+            String subject = this.jwtProvider.validateToken(header);
+            if (subject.isEmpty()){
+                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                return;
+            }
+            request.setAttribute("company_id", subject);
+            UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(subject, null, Collections.emptyList());
+            SecurityContextHolder.getContext().setAuthentication(auth);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
