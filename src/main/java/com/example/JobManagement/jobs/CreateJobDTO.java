185dccd1c4698b74be06563fd0e29bb0e98fe0a1
eriksgda <erik.ksgda@gmail.com>
2024-09-01 20:45:51 -0300

    create dto for create jobs

---
+++ src/main/java/com/example/JobManagement/jobs/CreateJobDTO.java
@@ -0,0 +1,13 @@
+package com.example.JobManagement.jobs;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CreateJobDTO {
+
+    private String description;
+    private String benefits;
+    private String level;
+}
