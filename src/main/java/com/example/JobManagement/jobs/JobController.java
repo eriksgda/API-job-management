5a10d7c53975a96c9dddb2ba96584ffabfe30cd2
eriksgda <erik.ksgda@gmail.com>
2024-09-01 20:46:32 -0300

    update create job endpoint in job controller

--- src/main/java/com/example/JobManagement/jobs/JobController.java
+++ src/main/java/com/example/JobManagement/jobs/JobController.java
@@ -1,5 +1,7 @@
 package com.example.JobManagement.jobs;
 
+import com.example.JobManagement.company.CompanyEntity;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -7,6 +9,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/job")
@@ -16,9 +20,18 @@ public class JobController {
     private JobService service;
 
     @PostMapping("/")
-    public ResponseEntity<Object> createJob(@Valid @RequestBody JobEntity job){
+    public ResponseEntity<Object> createJob(@Valid @RequestBody CreateJobDTO jobDTO, HttpServletRequest request){
         try {
+            Object companyId = request.getAttribute("company_id");
+            JobEntity job = JobEntity.builder()
+                    .companyId(UUID.fromString(companyId.toString()))
+                    .benefits(jobDTO.getBenefits())
+                    .description(jobDTO.getDescription())
+                    .level(jobDTO.getLevel())
+                    .build();
+
             this.service.createJob(job);
+
             return ResponseEntity.ok().body(job);
         } catch (Exception exception) {
             return ResponseEntity.badRequest().body("error");
