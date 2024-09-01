6f167f73cb7c2d54c0abb3bf169636dc822387c9
eriksgda <erik.ksgda@gmail.com>
2024-09-01 20:44:10 -0300

    create Authenticate candidate endpoint

--- src/main/java/com/example/JobManagement/security/SecurityAuthController.java
+++ src/main/java/com/example/JobManagement/security/SecurityAuthController.java
@@ -1,5 +1,8 @@
 package com.example.JobManagement.security;
 
+import com.example.JobManagement.candidate.CandidateAuthDTO;
+import com.example.JobManagement.candidate.CandidateAuthResponseDTO;
+import com.example.JobManagement.candidate.CandidateAuthService;
 import com.example.JobManagement.company.CompanyAuthDTO;
 import com.example.JobManagement.company.CompanyAuthService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,10 +22,23 @@ public class SecurityAuthController {
     @Autowired
     private CompanyAuthService companyAuthService;
 
+    @Autowired
+    private CandidateAuthService candidateAuthService;
+
     @PostMapping("/company")
     public ResponseEntity<Object> AuthenticateCompany(@RequestBody CompanyAuthDTO dto){
         try{
             String token = this.companyAuthService.authenticateCompany(dto);
+            return ResponseEntity.ok().body(token);
+        } catch (AuthenticationException exception){
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(exception.getMessage());
+        }
+    }
+
+    @PostMapping("/candidate")
+    public ResponseEntity<Object> AuthenticateCandidate(@RequestBody CandidateAuthDTO dto){
+        try{
+            CandidateAuthResponseDTO token = this.candidateAuthService.authenticateCandidate(dto);
             return ResponseEntity.ok().body(token);
         } catch (AuthenticationException exception){
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(exception.getMessage());
