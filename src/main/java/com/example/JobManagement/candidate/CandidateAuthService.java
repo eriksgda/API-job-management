e75208ed2c96f5ecd7d9852282e2f052098fd6e5
eriksgda <erik.ksgda@gmail.com>
2024-09-02 07:49:46 -0300

    create candidate auth service

---
+++ src/main/java/com/example/JobManagement/candidate/CandidateAuthService.java
@@ -0,0 +1,53 @@
+package com.example.JobManagement.candidate;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import javax.naming.AuthenticationException;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Date;
+import java.util.List;
+
+@Service
+public class CandidateAuthService {
+
+    @Value("$(security.token.secret.candidate)")
+    private String secretKey;
+
+    @Autowired
+    private CandidateRepository repository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public CandidateAuthResponseDTO authenticateCandidate(CandidateAuthDTO dto) throws AuthenticationException {
+        CandidateEntity candidate = this.repository.findByUsername(dto.username());
+        if(candidate == null){
+            throw new UsernameNotFoundException("Username or Password Incorrect.");
+        }
+
+        boolean passwordMatches = this.passwordEncoder.matches(dto.password(), candidate.getPassword());
+        if(!passwordMatches){
+            throw new AuthenticationException();
+        }
+
+        Algorithm algorithm = Algorithm.HMAC256(secretKey);
+        Instant expiresIn = Instant.now().plus(Duration.ofHours(2));
+        String token = JWT.create()
+                .withIssuer("candidate").withSubject(candidate.getId().toString())
+                .withClaim("roles", List.of("candidate"))
+                .withExpiresAt(expiresIn)
+                .sign(algorithm);
+
+        return CandidateAuthResponseDTO.builder()
+                .access_token(token)
+                .expired_in(expiresIn.toEpochMilli())
+                .build();
+    }
+}
