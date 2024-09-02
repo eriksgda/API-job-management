16a439b501dac7c78b3fa87ec35ea44210acbfb8
eriksgda <erik.ksgda@gmail.com>
2024-09-02 07:49:19 -0300

    update candidate service, now the password is encoded

--- src/main/java/com/example/JobManagement/candidate/CandidateService.java
+++ src/main/java/com/example/JobManagement/candidate/CandidateService.java
@@ -2,6 +2,7 @@ package com.example.JobManagement.candidate;
 
 import com.example.JobManagement.exceptions.UserOrEmailAlreadyExistException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,10 +11,16 @@ public class CandidateService {
     @Autowired
     private CandidateRepository  repository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public CandidateEntity createUser(CandidateEntity candidate){
         if (this.repository.findByUsernameOrEmail(candidate.getUsername(), candidate.getEmail()) != null){
             throw new UserOrEmailAlreadyExistException();
         }
+        String encodedPassword = this.passwordEncoder.encode(candidate.getPassword());
+        candidate.setPassword(encodedPassword);
+
         return this.repository.save(candidate);
     }
 }
