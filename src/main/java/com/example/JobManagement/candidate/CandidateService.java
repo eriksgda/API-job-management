75dddd197d457b313422a9f3b0ab349b6826e8b9
eriksgda <erik.ksgda@gmail.com>
2024-09-03 18:59:49 -0300

    create get profile user in candidate service

--- src/main/java/com/example/JobManagement/candidate/CandidateService.java
+++ src/main/java/com/example/JobManagement/candidate/CandidateService.java
@@ -2,8 +2,12 @@ package com.example.JobManagement.candidate;
 
 import com.example.JobManagement.exceptions.UserOrEmailAlreadyExistException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class CandidateService {
@@ -22,5 +26,21 @@ public class CandidateService {
         candidate.setPassword(encodedPassword);
 
         return this.repository.save(candidate);
+    }
+
+    public CandidateProfileResponseDTO getProfileUser(UUID candidateId){
+        Optional<CandidateEntity> candidate = this.repository.findById(candidateId);
+
+        if (candidate.isEmpty()){
+            throw new UsernameNotFoundException("User not found.");
+        }
+
+        return CandidateProfileResponseDTO.builder()
+                .id(candidate.get().getId())
+                .name(candidate.get().getName())
+                .username(candidate.get().getUsername())
+                .email(candidate.get().getEmail())
+                .description(candidate.get().getDescription())
+                .build();
     }
 }
