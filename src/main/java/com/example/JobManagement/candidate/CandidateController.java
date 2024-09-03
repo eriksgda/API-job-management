5328af4a988dbadf78854ca2b1e2afdaab0b9881
eriksgda <erik.ksgda@gmail.com>
2024-09-03 18:58:16 -0300

    add get profile endpoint in candidate controller

--- src/main/java/com/example/JobManagement/candidate/CandidateController.java
+++ src/main/java/com/example/JobManagement/candidate/CandidateController.java
@@ -1,13 +1,15 @@
 package com.example.JobManagement.candidate;
 
 import com.example.JobManagement.exceptions.UserOrEmailAlreadyExistException;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/candidate")
@@ -21,8 +23,25 @@ public class CandidateController {
         try{
             CandidateEntity result =  this.service.createUser(candidate);
             return ResponseEntity.ok().body(result);
-        }catch(UserOrEmailAlreadyExistException exception){
+        }catch (UserOrEmailAlreadyExistException exception){
             return ResponseEntity.badRequest().body(exception.getMessage());
+        }catch (Exception exception){
+            return ResponseEntity.internalServerError().body("error");
+        }
+    }
+
+    @GetMapping("/")
+    @PreAuthorize("hasRole('CANDIDATE')")
+    public ResponseEntity<Object> getProfileCandidate(HttpServletRequest request){
+        Object id = request.getAttribute("candidate_id");
+
+        try {
+            CandidateProfileResponseDTO profile = this.service.getProfileUser(UUID.fromString(id.toString()));
+            return ResponseEntity.ok().body(profile);
+        } catch (UsernameNotFoundException exception){
+            return ResponseEntity.badRequest().body(exception.getMessage());
+        }catch (Exception exception){
+            return ResponseEntity.internalServerError().body("error");
         }
     }
 }
