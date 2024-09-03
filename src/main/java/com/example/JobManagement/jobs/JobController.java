e9f2fab36216e34942e6e6cac63c229f58ef4873
eriksgda <erik.ksgda@gmail.com>
2024-09-03 19:01:58 -0300

    update endpoint in job controller

--- src/main/java/com/example/JobManagement/jobs/JobController.java
+++ src/main/java/com/example/JobManagement/jobs/JobController.java
@@ -5,6 +5,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,13 +14,14 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/job")
+@RequestMapping("/company/job")
 public class JobController {
 
     @Autowired
     private JobService service;
 
     @PostMapping("/")
+    @PreAuthorize("hasHole('COMPANY')")
     public ResponseEntity<Object> createJob(@Valid @RequestBody CreateJobDTO jobDTO, HttpServletRequest request){
         try {
             Object companyId = request.getAttribute("company_id");
