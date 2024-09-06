c55c23726be01c14609763c792cd3f1d206f1354
eriksgda <erik.ksgda@gmail.com>
2024-09-06 19:37:17 -0300

    add swagger infos

--- src/main/java/com/example/JobManagement/JobManagementApplication.java
+++ src/main/java/com/example/JobManagement/JobManagementApplication.java
@@ -1,8 +1,17 @@
 package com.example.JobManagement;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Cat Job Management",
+				description = "API for job management *Only for Cats*",
+				version = "1.0"
+		)
+)
 @SpringBootApplication
 public class JobManagementApplication {
 
