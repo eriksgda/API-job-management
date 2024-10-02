2b402023a4d6597627eeb9b7d5f799976a5e58f7
eriksgda <erik.ksgda@gmail.com>
2024-10-02 13:18:38 -0300

    create test Apply Job Return Job Not Found

--- src/test/java/com/example/JobManagement/modules/candidate/service/CandidateServiceTest.java
+++ src/test/java/com/example/JobManagement/modules/candidate/service/CandidateServiceTest.java
@@ -1,7 +1,9 @@
 package com.example.JobManagement.modules.candidate.service;
 
+import com.example.JobManagement.candidate.CandidateEntity;
 import com.example.JobManagement.candidate.CandidateRepository;
 import com.example.JobManagement.candidate.CandidateService;
+import com.example.JobManagement.exceptions.JobNotFoundException;
 import com.example.JobManagement.exceptions.UserNotFoundException;
 import com.example.JobManagement.jobs.JobRepository;
 import org.junit.jupiter.api.Assertions;
@@ -11,6 +13,11 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class CandidateServiceTest {
@@ -29,5 +36,17 @@ public class CandidateServiceTest {
     void testApplyJobReturnCandidateNotFound(){
         Assertions.assertThrows(UserNotFoundException.class,
                 () -> service.applyJob(null, null));
+    }
+
+    @Test
+    @DisplayName("Apply job should return job not found exception if job don't exists.")
+    void testApplyJobReturnJobNotFound(){
+        UUID candidateId = UUID.randomUUID();
+        CandidateEntity candidate = new CandidateEntity();
+        candidate.setId(candidateId);
+
+        when(candidateRepository.findById(candidateId)).thenReturn(Optional.of(candidate));
+        Assertions.assertThrows(JobNotFoundException.class,
+                () -> service.applyJob(candidateId, null));
     }
 }
