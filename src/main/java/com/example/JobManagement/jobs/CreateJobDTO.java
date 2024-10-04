b51aa9d5b62ccffc672d34f6a3b12df86c1eb454
eriksgda <erik.ksgda@gmail.com>
2024-10-04 18:29:18 -0300

    add builder annotation in create job DTO

--- src/main/java/com/example/JobManagement/jobs/CreateJobDTO.java
+++ src/main/java/com/example/JobManagement/jobs/CreateJobDTO.java
@@ -1,12 +1,10 @@
 package com.example.JobManagement.jobs;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class CreateJobDTO {
