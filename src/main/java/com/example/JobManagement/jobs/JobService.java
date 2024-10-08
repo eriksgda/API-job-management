c17e468450a92427e8e9fea7419c722297b36f96
eriksgda <erik.ksgda@gmail.com>
2024-10-08 12:31:00 -0300

    add conditions in job service

--- src/main/java/com/example/JobManagement/jobs/JobService.java
+++ src/main/java/com/example/JobManagement/jobs/JobService.java
@@ -1,7 +1,12 @@
 package com.example.JobManagement.jobs;
 
+import com.example.JobManagement.company.CompanyEntity;
+import com.example.JobManagement.company.CompanyRepository;
+import com.example.JobManagement.exceptions.CompanyNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class JobService {
@@ -9,7 +14,15 @@ public class JobService {
     @Autowired
     private JobRepository repository;
 
+    @Autowired
+    private CompanyRepository companyRepository;
+
     public JobEntity createJob(JobEntity job) {
+        Optional<CompanyEntity> company = this.companyRepository.findById(job.getCompanyId());
+        if (company.isEmpty()){
+            throw new CompanyNotFoundException();
+        }
+
         return this.repository.save(job);
     }
 }
